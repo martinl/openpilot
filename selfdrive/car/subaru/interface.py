a37c951f497083c3e8e1fcc4ea03eec6c63ad606
Crispin Flowerday <crispin@theflowerdays.com>
2021-04-18 20:35:36 -0700

    Add some fixes for test & forester hybrid
    The timeout used in test_startup isn't long enough with the
    subaru fpv2 added, and add a small fix for the forester hybrid,
    although that car support doesn't fully work (dbc errors)

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -52,7 +52,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
 
-    if candidate == CAR.FORESTER:
+    if candidate in [CAR.FORESTER, CAR.FORESTER_HYBRID]:
       ret.mass = 1568. + STD_CARGO_KG
       ret.wheelbase = 2.67
       ret.centerToFront = ret.wheelbase * 0.5
