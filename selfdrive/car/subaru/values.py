40224be0566bea44740411e7cb46ca64034b5479
lonelyju <111476834+lonelyju@users.noreply.github.com>
2022-09-13 17:02:15 +0800

    Update value.py
    Added fingerprint of 2020 Subaru Levorg 2.0 VMG

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -411,18 +411,22 @@ FW_VERSIONS = {
      # Ecu, addr, subaddr: ROM ID
      (Ecu.esp, 0x7b0, None): [
        b'j\x9c\x92\002',
+       b'\x9a\x95\x92\x00',
      ],
      (Ecu.eps, 0x746, None): [
        b'Z\xb0\000\000',
      ],
      (Ecu.fwdCamera, 0x787, None): [
        b'\000\000c\xd3\000\000\000\000',
+       b'\x00\x00eO\x00\x00\x00\x00',
      ],
      (Ecu.engine, 0x7e0, None): [
        b'\xf1\x82\xa6\000\xa1t\a',
+       b'\xf1\x82\xcdi\xa00\x07',
      ],
      (Ecu.transmission, 0x7e1, None): [
        b'\xcb\xd9\000p\000',
+       b'\xce\xed\xf0`\x00',
      ],
   },
   CAR.OUTBACK: {
