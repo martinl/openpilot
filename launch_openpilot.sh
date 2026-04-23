b1576aa4603cd473feff107233e08c18f6065cc0
Martin Lillepuu <martin@mlp.ee>
2026-04-23 11:35:51 +0300

    Use konik stable as log server, skip FW query

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-SKIP_FW_QUERY=1 FINGERPRINT="BYD_SEALION_7" exec ./launch_chffrplus.sh
+export API_HOST=https://api.konik.ai/
+export ATHENA_HOST=wss://athena.konik.ai
+
+export SKIP_FW_QUERY=1
+export FINGERPRINT="BYD_SEALION_7"
+
+exec ./launch_chffrplus.sh
